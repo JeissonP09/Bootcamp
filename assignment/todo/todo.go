50985d737e3cfea229301f6e3bec710b6f13af14
Jeisson Pinilla <jpinilla@sistemasenlinea.com.co>
2025-06-09 15:44:17 -0500

    Refactor methods

--- assignment/todo/todo.go
+++ assignment/todo/todo.go
@@ -17,25 +17,25 @@ type List struct {
 }
 
 func (l *List) Add(title string) {
-	newItem := item{Title: title, Done: false}
-	*l = append(*l, newItem)
+	newItem := item{Task: title, Done: false}
+	l.items = append(l.items, newItem)
 }
 
 func (l *List) Complete(index int) error {
-	if index < 0 || index >= len(*l) {
-		return fmt.Errorf("Invalid index")
+	if index < 0 || index >= len(l.items) {
+		return fmt.Errorf("invalid index")
 	}
-	(*l)[index].Done = true
-	(*l)[index].CompletedAt = time.Now()
+	l.items[index].Done = true
+	l.items[index].CompletedAt = time.Now()
 
 	return nil
 }
 
 func (l *List) Delete(index int) error {
-	if index < 0 || index >= len(*l) {
-		return fmt.Errorf("Invalid index")
+	if index < 0 || index >= len(l.items) {
+		return fmt.Errorf("invalid index")
 	}
-	*l = append((*l)[:index], (*l)[index+1:]...)
+	l.items = append(l.items[:index], l.items[index+1:]...)
 
 	return nil
 }

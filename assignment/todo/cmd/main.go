3eba497734fed3cf6233e7bbebf28378181205ba
Jeisson Pinilla <jpinilla@sistemasenlinea.com.co>
2025-06-17 18:11:41 -0500

    Refactor: create const NoAction and change of "switch" for "else if"

--- assignment/todo/cmd/main.go
+++ assignment/todo/cmd/main.go
@@ -8,7 +8,10 @@ import (
 	"todo"
 )
 
-const fileName = ".todo.json"
+const (
+	fileName = ".todo.json"
+	NoAction = -1
+)
 
 func main() {
 	add := flag.Bool("add", false, "Add a new task")
@@ -18,6 +21,7 @@ func main() {
 	flag.Parse()
 
 	var list todo.List
+
 	if err := list.Get(fileName); err != nil {
 		fmt.Fprintf(os.Stderr, "Error getting tasks: %v\n", err)
 		os.Exit(1)
@@ -25,27 +29,26 @@ func main() {
 
 	args := flag.Args()
 
-	switch {
-	case *add || (len(args) > 0 && *complete == -1 && *delete == -1):
+	if *add || (len(args) > 0 && *complete == NoAction && *delete == NoAction) {
 		task := strings.Join(args, " ")
 		if task == "" {
 			fmt.Fprintf(os.Stderr, "Task cannot be empty")
 			os.Exit(1)
 		}
 		list.Add(task)
-	case *complete >= 0:
+	} else if *complete >= 0 {
 		err := list.Complete(*complete - 1)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error completing task: %v\n", err)
 			os.Exit(1)
 		}
-	case *delete >= 0:
+	} else if *delete >= 0 {
 		err := list.Delete(*delete - 1)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error deleting task: %v\n", err)
 			os.Exit(1)
 		}
-	default:
+	} else {
 		for _, item := range list {
 			fmt.Println(item.Task)
 		}

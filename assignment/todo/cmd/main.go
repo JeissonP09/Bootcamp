194bd5b6601f67a741c54edc8975fd0ea3c1acaa
Jeisson Pinilla <jpinilla@sistemasenlinea.com.co>
2025-06-20 10:00:40 -0500

    Refactor "main.go"

--- assignment/todo/cmd/main.go
+++ assignment/todo/cmd/main.go
@@ -10,10 +10,10 @@ import (
 const fileName = ".todo.json"
 
 func main() {
-	listFlag := flag.Bool("list", false, "List incomplete tasks")
-	taskFlag := flag.String("task", "", "Add a new task")
-	completeFlag := flag.Int("complete", -1, "Complete a task")
-	deleteFlag := flag.Int("delete", -1, "Delete a task")
+	list := flag.Bool("list", false, "List incomplete tasks")
+	task := flag.String("task", "", "Add a new task")
+	complete := flag.Int("complete", -1, "Complete a task")
+	delete := flag.Int("delete", -1, "Delete a task")
 	flag.Parse()
 
 	var l todo.List
@@ -23,17 +23,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	commandExecuted := false
-
-	if *listFlag {
+	if *list {
 		for _, item := range l {
 			if !item.Done {
 				fmt.Printf("Title: %s, Done: %t, CreatedAt: %s, CompletedAt: %s", item.Task, item.Done, item.CreatedAt, item.CompletedAt)
 			}
 		}
-		commandExecuted = true
-	} else if *completeFlag != -1 {
-		if err := l.Complete(*completeFlag - 1); err != nil {
+		return
+	}
+
+	if *complete != -1 {
+		if err := l.Complete(*complete - 1); err != nil {
 			fmt.Fprintf(os.Stderr, "Error completing task: %v\n", err)
 			os.Exit(1)
 		}
@@ -41,9 +41,11 @@ func main() {
 			fmt.Fprintf(os.Stderr, "Error saving ToDo list: %v\n", err)
 			os.Exit(1)
 		}
-		commandExecuted = true
-	} else if *deleteFlag != -1 {
-		if err := l.Delete(*deleteFlag - 1); err != nil {
+		return
+	}
+
+	if *delete != -1 {
+		if err := l.Delete(*delete - 1); err != nil {
 			fmt.Fprintf(os.Stderr, "Error deleting task: %v\n", err)
 			os.Exit(1)
 		}
@@ -51,22 +53,22 @@ func main() {
 			fmt.Fprintf(os.Stderr, "Error saving ToDo list: %v\n", err)
 			os.Exit(1)
 		}
-		commandExecuted = true
-	} else if *taskFlag != "" {
-		if *taskFlag == "" {
+		return
+	}
+
+	if *task != "" {
+		if *task == "" {
 			fmt.Fprintf(os.Stderr, "Task cannot be empty")
 			os.Exit(1)
 		}
-		l.Add(*taskFlag)
+		l.Add(*task)
 		if err := l.Save(fileName); err != nil {
 			fmt.Fprintf(os.Stderr, "Error saving ToDo list: %v\n", err)
 			os.Exit(1)
 		}
-		commandExecuted = true
+		return
 	}
 
-	if !commandExecuted {
-		fmt.Fprintf(os.Stderr, "You must provide a valid command")
-		os.Exit(1)
-	}
+	fmt.Fprintf(os.Stderr, "You must provide a valid command")
+	os.Exit(1)
 }

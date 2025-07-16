d21d20303cf964132f903563b23750f1a533412f
Jeisson Pinilla <jpinilla@sistemasenlinea.com.co>
2025-07-16 11:46:23 -0500

    refactor: the functions were created "textReply" and "errorReply", and changes in "rootHandler"

--- assignment/server/handlers.go
+++ assignment/server/handlers.go
@@ -5,11 +5,12 @@ import "net/http"
 // Created controller "rootHandler"
 func rootHandler(w http.ResponseWriter, r *http.Request){
 	// Validates if the path is correct "/"
+	// If there is an error we use the errorReply function
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		errorReply(w, r, http.StatusNotFound, "404 page not found")
 		return
 	}
 
-	// Response
-	w.Write([]byte("Hello World!!"))
+	// Response with textReply function
+	textReply(w, r, http.StatusOK, "Hello World!!")
 }
